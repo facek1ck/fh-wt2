8693877e635ce3a335831191b0c8b10ad77d4de9
Gabriel Asherov <if18b101@technikum-wien.at>
2019-05-19 23:46:12 +0200

    rewrite test list logic  - implement states
    Signed-off-by: Gabriel Asherov <if18b101@technikum-wien.at>

--- my-app/src/components/test-list/test-list.jsx
+++ my-app/src/components/test-list/test-list.jsx
@@ -3,59 +3,80 @@ import { Card, Avatar, Button } from 'antd';
 import { observer, inject } from 'mobx-react';
 import './test-list.less';
 import axios from 'axios';
-import tmp from '../app/specs/quizzes/*.json';
+import files from '../app/specs/quizzes/*.json';
 
 const { Meta } = Card;
 
 @inject("store")
 export default class TestList extends Component {
-    render() {
-        const store = this.props.store;
-        let tests;
-        if(store.user.online){
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            tests: []
+        }
+    }
+
+    componentDidMount() {
+        this.getTests()
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.store.online !== this.props.store.online) {
+            this.getTests()
+        }
+    }
+
+    getTests() {
+        const { store } = this.props
+        if(store.online){
             axios.get('http://gabriels-macbook.local:3000/users/'+store.user.id+'/open').then(response => {
-            tests=response;
+                this.setState({
+                    ...this.state,
+                    tests: response.data
+                })
+            //tmp=response.data;
             })
         } else {
-             tests = store.user.tests.filter(x => !store.user.taken.includes(x));
-             console.log(tests);
-             let tmp2=[];
-             tests.forEach(element => {
-                Object.keys(tmp).forEach(
-                    key => {
-                        if(element == key){
-                            console.log(key);
-                            tmp2.push(tmp[key]);
-                        }
-                    }
-                )
-             });
-             console.log(tmp2);
-        }
-        
+            const tests = store.user.tests
+                .filter(x => !store.user.taken.includes(x))
+                .filter(test => Object.keys(files).includes(test))
+                .map(test => files[test])
 
-        var renderedOutput = tests.map(test => 
+            this.setState({
+                ...this.state,
+                tests
+            })
+        }
+    }
+
+    renderTests() {
+        return this.state.tests.map(test => 
             <Card
+            key={test.name}
             style={{ width: 450, marginTop: 16 }}
             extra={
                 <Button type="primary">
                     Take Test
                 </Button>
             }
-            title={test} 
+            title={test.name} 
         >
             <Meta
                 avatar={
                     <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>A</Avatar>
                 }
-                description="seas"
+                description={test.description}
             /> 
         </Card>
         );
+    }
+    render() {
+        const tests = this.renderTests()
 
         return(  
             <div>
-            {renderedOutput}
+            {tests}
             </div> 
         )
     }
