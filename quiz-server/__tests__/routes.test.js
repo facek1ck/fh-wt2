7011173bf695a65d3590610b924e1d1871b8a0f2
Michael Weissenboeck <weissenboeck.michael@icloud.com>
2019-05-21 18:05:58 +0200

    changed rest api tests

--- quiz-server/__tests__/routes.test.js
+++ quiz-server/__tests__/routes.test.js
@@ -18,24 +18,29 @@ afterAll(() => {
     console.log('server closed!');
 });
 
-/* describe('basic REST GET tests', () => {
+
+
+describe('basic REST GET tests', () => {
 
     test('get home route GET /', async () => {
         const response = await request(server).get('/');
         expect(response.status).toEqual(200);
-        expect(response.text).toContain('Hello world');
+        expect(response.text).toContain('Server is alive and running');
     });
 
     test('get all users GET /users', async () => {
-        const response = await request(server).get('/users');
+        const
+            users = "Gabriel",
+            response = await request(server).get('/users');
+
+        expect(response.text).toContain(users);
         expect(response.status).toEqual(200);
-        expect(response.text).toContain('Gabriel');
     });
 
     test('get one user GET /users/:userid', async () => {
         const response = await request(server).get('/users/1');
         expect(response.status).toEqual(200);
-        expect(response.text).toContain('Gabriel');
+        expect(response.text).toContain('Asherov');
     });
 
     test('get open tests of user GET /users/:userid/open', async () => {
@@ -45,15 +50,20 @@ afterAll(() => {
     });
 
     test('get user by name GET /tests/:name', async () => {
-        const response = await request(server).get('/tests/Essen');
+        const 
+            question = "Bliny sind die russische Version von ... ?"; 
+            response = await request(server).get('/tests/Essen');
+
         expect(response.status).toEqual(200);
-        expect(response.text).toContain('Was ist die Hauptzutat von Guacamole?","Wie hoch ist der Wassergehalt in Champignons?');
+        expect(response.text).toContain(question);
     });
 
     test('get sample question GET /tests/:name/questions', async () => {
-        const response = await request(server).get('/tests/Essen/questions');
+        const 
+            question = "Sie möchten vermeiden, Eier von Legebatteriehennen zu kaufen. Welche Ziffer auf dem Ei steht für Käfighaltung?",
+            response = await request(server).get('/tests/Essen/questions');
         expect(response.status).toEqual(200);
-        expect(response.text).toContain('In vielen Lebensmitteln steckt Ascorbinsäure. Was war das doch gleich?');
+        expect(response.text).toContain(question);
     });
 
     test('get specific question by id GET /tests/:name/questions/:id', async () => {
@@ -62,11 +72,13 @@ afterAll(() => {
         expect(response.text).toContain('Vitamin A');
     });
 
-    test('get currently online users GET /requests/online', async () => {
+
+    //online user test - cannot check cause user may be offline
+    /* test('get currently online users GET /requests/online', async () => {
         const response = await request(server).get('/requests/online');
         expect(response.status).toEqual(200);
         expect(response.text).toContain('2');
-    });
+    }); */
 
     test('get GET /evaluation/:testID', async () => {
         const response = await request(server).get('/evaluation/1');
@@ -74,29 +86,32 @@ afterAll(() => {
         expect(response.text).toContain('');
     });
 
-}); */
+
+}); 
 
 
 describe('basic REST POST tests', () => {
 
-    const filePath = path.join(__dirname, '../testFiles/test.file');
-    let testFilePath = null;
+    
+     const filePath = path.join(__dirname, '../testFiles/test.file');
+     let testFilePath = null;
+ 
+     // Upload first test file to CDN
+     it('should upload the test file to CDN', () =>
+         // Test if the test file is exist
+         fs.exists(filePath)
+             .then((exists) => {
+                 if (!exists) throw new Error('file does not exist');
+                 return request(server)
+                     .post('/tests/Asherov/upload')
+                     .attach('55CHbmatnFYH6UYy', filePath)
+                     .then((res) => {
+                         const { success, message, filePath } = res.body;
+                         expect(success).toBeTruthy();
+                         expect(message).toBe('successfully uploaded result');
+                     })
+                     .catch(err => console.log(err));
+             }));
+             
 
-    // Upload first test file to CDN
-    it('should upload the test file to CDN', () =>
-        // Test if the test file is exist
-        fs.exists(filePath)
-            .then((exists) => {
-                if (!exists) throw new Error('file does not exist');
-                return request(server)
-                    .post('/tests/Asherov/upload')
-                    .attach('55CHbmatnFYH6UYy', filePath)
-                    .then((res) => {
-                        const { success, message, filePath } = res.body;
-                        expect(success).toBeTruthy();
-                        expect(message).toBe('successfully uploaded result');
-                    })
-                    .catch(err => console.log(err));
-            }));
-
-});
+}); 
